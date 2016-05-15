b3fcfd075fc7a30e65831dc43d585d53cfd41003
Jackson Cafazzo <doctorjuno@gmail.com>
2016-05-15 14:33:16 -0700

    reference changes

--- src/LarchProvisionsWebsite/wwwroot/_references.js
+++ src/LarchProvisionsWebsite/wwwroot/_references.js
@@ -1,6 +1,5 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="../gulpfile.js" />
-/// <reference path="js/current_menu.js" />
 /// <reference path="js/fb-sdk.js" />
 /// <reference path="js/materialize.es5.js" />
 /// <reference path="js/site.js" />
